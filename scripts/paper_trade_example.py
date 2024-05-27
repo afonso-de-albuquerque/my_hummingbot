03f3cdaae19d38196b3593d07b528e544b7ec008
Edward Fu <foolq9@gmail.com>
2024-05-28 02:02:25 +0800

    Bug fixing

--- scripts/paper_trade_example.py
+++ scripts/paper_trade_example.py
@@ -1,11 +1,11 @@
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 class MyStrategy(ScriptStrategyBase):
-    markets = { "binance_paper_trade": {"ETH-USDT"}}
-    def __init__(self):
-        super().__init__()
+    markets = {
+        "binance_paper_trade": {"ETH-USDT"},
+        "kucoin_paper_trade": {"ETH-USDT"}
+    }
 
     def on_tick(self):
         for connector_name, connector in self.connectors.items():
             self.logger().info(f"Current price on {connector_name}: {connector.get_price()}")
-
